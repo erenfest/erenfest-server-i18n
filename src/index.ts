8f91831939863377289188f728215e19fd829d32
Danuel <public.danuel@gmail.com>
2019-07-21 04:13:57 +0900

    CORS 이슈 대응

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import Express from 'express'
+import CORS from 'cors'
 
 const app = Express()
 
+app.use(CORS())
 app.use('/i18n', Express.static('i18n'))
 
 app.listen(process.env.PORT, () => console.log(`::: RUNNING i18n delivery server on ${process.env.PORT} port :::`))
