eb433ab646d36e6706ab2c1c19fbb83d206d1aaf
Danuel <public.danuel@gmail.com>
2019-07-31 00:31:40 +0900

    DX를 위한 커맨드라인 기능 추가

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,43 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+import Chalk from 'chalk'
+import Inquirer from 'inquirer'
+import glob from 'glob'
 import Express from 'express'
 import CORS from 'cors'
+
+import { languages } from './languages'
 
 const app = Express()
 
 app.use(CORS())
-app.use('/i18n', Express.static('i18n'))
+app.use(Express.static('i18n'))
 
-app.listen(process.env.PORT, () => console.log(`::: RUNNING i18n delivery server on ${process.env.PORT} port :::`))
+app.listen(process.env.PORT, async () => {
+  console.clear()
+  console.log(Chalk`{green Compiled successfully!}\n`)
+  console.log(Chalk`You can now view {bold erenfest-server-i18n} in the browser\n`)
+  console.log(Chalk`  {bold Local}: http://localhost:${process.env.PORT!}/\n`)
+
+  while (true) {
+    await Inquirer.prompt({
+      name: 'language',
+      message: 'Which language will you check?',
+      type: 'list',
+      choices: glob
+        .sync('i18n/*/')
+        .map(directory => directory.slice(5, -1))
+        .filter(languages.hasCode)
+        .map<string>(languages.toName as any)
+    }).then(async ({ language }: any) => {
+      const fileNameList = glob.sync(`i18n/${languages.toCode(language)}/**/*.json`, { nodir: true, nounique: true })
+
+      console.log(Chalk`\n${language} has {bold ${fileNameList.length.toString()}} translations\n`)
+
+      if (fileNameList.length) {
+        fileNameList.forEach(fileName => console.log(`- http://localhost:${process.env.PORT}/${fileName.slice(5)}\n`))
+      }
+    })
+  }
+})
